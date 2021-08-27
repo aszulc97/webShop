b338dda87a8c8dc3570c1bf8f701b641a3107bf2
Agata Szulc <agataszulc97@gmail.com>
2021-08-27 12:52:39 +0200

    First commit - basic look

---
+++ product.js
@@ -0,0 +1,16 @@
+const productId = 1529;
+const url = "https://kea-alt-del.dk/t7/api/products/1529";
+
+fetch (url)
+    .then(res=>res.json())
+    .then(data=>showProduct(data));
+
+
+function showProduct(product) {
+    console.log(product);
+    document.querySelector(".gender").textContent=product.gender;
+    document.querySelector(".brand").textContent=product.brandname;
+    document.querySelector(".season").textContent=product.season;
+    document.querySelector(".productName").textContent=product.productdisplayname;
+    document.querySelector(".productImg").src=`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
+}
