cb89845dc9dfc4190d3df56b11653bf63213d5a8
Agata Szulc <agataszulc97@gmail.com>
2021-08-31 13:29:30 +0200

    product list works dynamically

--- product.js
+++ product.js
@@ -1,16 +1,19 @@
-const productId = 1529;
-const url = "https://kea-alt-del.dk/t7/api/products/1529";
+const urlParams = new URLSearchParams(window.location.search);
+const productID = urlParams.get("id");
+const url = "https://kea-alt-del.dk/t7/api/products/" + productID;
 
-fetch (url)
-    .then(res=>res.json())
-    .then(data=>showProduct(data));
-
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
 
 function showProduct(product) {
-    console.log(product);
-    document.querySelector(".gender").textContent=product.gender;
-    document.querySelector(".brand").textContent=product.brandname;
-    document.querySelector(".season").textContent=product.season;
-    document.querySelector(".productName").textContent=product.productdisplayname;
-    document.querySelector(".productImg").src=`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
+  console.log(product);
+  document.querySelector(".gender").textContent = product.gender;
+  document.querySelector(".brand").textContent = product.brandname;
+  document.querySelector(".season").textContent = product.season;
+  document.querySelector(".productName").textContent =
+    product.productdisplayname;
+  document.querySelector(
+    ".productImg"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
 }
---
+++ productlist.js
@@ -0,0 +1,18 @@
+// const urlParams = new URLSearchParams(window.location.search);
+// const productID = urlParams.get("id");
+const url = "https://kea-alt-del.dk/t7/api/products/";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => data.forEach(showProduct));
+
+function showProduct(product) {
+  const template = document.querySelector("template").content;
+  const clone = template.cloneNode(true);
+  clone.querySelector("h2").textContent = product.productdisplayname;
+  clone.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  const parent = document.querySelector("div");
+  parent.appendChild(clone);
+}
