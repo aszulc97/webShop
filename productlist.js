ed14aac67fefa3795c424544341832f2624caac3
Agata Szulc <agataszulc97@gmail.com>
2021-08-31 13:45:13 +0200

    products linked to productlist

--- productlist.js
+++ productlist.js
@@ -1,6 +1,6 @@
 // const urlParams = new URLSearchParams(window.location.search);
 // const productID = urlParams.get("id");
-const url = "https://kea-alt-del.dk/t7/api/products/";
+const url = "https://kea-alt-del.dk/t7/api/products?limit=100";
 
 fetch(url)
   .then((res) => res.json())
@@ -9,6 +9,7 @@ fetch(url)
 function showProduct(product) {
   const template = document.querySelector("template").content;
   const clone = template.cloneNode(true);
+  clone.querySelector("a").href = "product.html?id=" + product.id;
   clone.querySelector("h2").textContent = product.productdisplayname;
   clone.querySelector(
     "img"
